a49144f23b12a8120a7bf847951a9d955eb9f3c8
Anas Mohamed <49694746+anas2479@users.noreply.github.com>
2022-12-16 08:40:01 -0600

    add FQAs

---
+++ src/components/Fqas.svelte
@@ -0,0 +1,17 @@
+
+<div class="max-w-6xl mx-auto p-4">
+    <h2 class="text-5xl text-gray-700 font-heading mb-4">Frequently Asked Questions</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {#each questions as question}
+            <div class="bg-white shadow-md rounded-lg p-4">
+                <h3 class="text-xl font-bold mb-2">{question.question}</h3>
+                <p>{question.answer}</p>
+            </div>
+        {/each}
+    </div>
+</div>
+
+
+<script>
+    import questions from "../questions";
+</script>
---
+++ src/questions.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        question: "If I’m not a member, can I still use the space and resources?",
+        answer: `Yes! Ohalanʌ́ht has a rental system for some of our equipment, computers, and our studio/sweep. Our members get first dibs on the calendar, but we open rentals to the public on the first of every month.
+        `,
+    },
+    {
+        question: `How will the money raised in your campaign be used?`,
+        answer:`In order to establish our studio, we will need to purchase computers, software, printers, ink, and all the fun stuff that makes a studio run. All in all, we need about $55,000 to outfit our studio. The remaining amount of our $70,000 goal will cover the first six months of rent as we hit the ground running.`,
+    },
+    {
+        question: `I’m not an artist, but I love your work! How can I get involved?`,
+        answer: `We’ll host premiere screenings and gallery shows of our members’ work, as well as send out a monthly newsletter with project and publication updates.`,
+    },
+    {
+        question: `I’m not an artist yet, how can I get started?`,
+        answer: `As part of our commitment to the community, Ohalanʌ́ht plans to hold 1-2 small introductory classes a month on the basic principles of animation, photography, comic art, and whatever else our artists have gotten their hands on. Skills are more fun when you share them! Taking a class or coming to an event is a great way to network with us. Don’t be afraid to reach out.`
+    }
+]
